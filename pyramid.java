2d0f69318bfc350f9c4792068a0cf6d1a8dc2746
nandeswarip852 <nandeswarip852@gmail.com>
2025-08-14 11:40:15 +0530

    Add files via upload

---
+++ pyramid.java
@@ -0,0 +1,17 @@
+public class PyramidPattern {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for(int i = 1; i <= rows; i++) {
+            // Print spaces
+            for(int j = i; j < rows; j++) {
+                System.out.print("  ");
+            }
+            // Print stars
+            for(int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
